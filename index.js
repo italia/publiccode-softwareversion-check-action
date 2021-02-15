b4352a7a68816ad0a389eecc4fc5a70f544619f8
Andrea Stagi <stagi.andrea@gmail.com>
2021-02-15 22:15:11 +0100

    feature: main functionality

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const https = require("https")
+const core = require("@actions/core")
+const yaml = require('js-yaml');
+const fs = require('fs');
+const simpleGit = require('simple-git');
 
-const githubToken = core.getInput('github_token');
-const repo = core.getInput('repo');
 
-const requestOptions = {
-  hostname: 'api.github.com',
-  path: `/repos/${repo}`,
-  headers: { 
-    'User-Agent': 'Mozilla/5.0',
+let main = async () => {
+  try {
+    const git = simpleGit();
+    const tag = (await git.tags()).latest
+
+    core.info(`Current tag is ${tag}`)
+
+    const doc = yaml.load(fs.readFileSync('publiccode.yml', 'utf8'))
+    if (doc.softwareVersion !== tag) {
+      git.addConfig('user.name', 'Publiccode bot') 
+      git.addConfig('user.email', 'italia@gmail.com')
+      doc.softwareVersion = tag
+      let yamlStr = yaml.dump(doc)
+      fs.writeFileSync('publiccode.yaml', yamlStr, 'utf8')
+      git.add('./*').commit(`feat: bump publiccode.yml to version ${tag}`)
+    }
   }
-};
-if (githubToken) {
-  console.log("Use GITHUB_TOKEN to get release data.");
-  requestOptions.headers['Authorization'] = `token: ${githubToken}`
-} else {
-  console.log("GITHUB_TOKEN is not available. Subsequent GitHub API call may fail due to API limit.");
+  catch (error) {
+    core.setFailed(error)
+  }
 }
 
-const repoRequest = https.request(requestOptions, res => {
-  let responseData = '';
-  res.on('data', (d) => {
-    responseData += d;
-  })
-  res.on('end', () => {
-    const response = JSON.parse(responseData);
-    const stars = response.stargazers_count;
-    const license = response.license.name;
-    console.log(`Repo has ${stars} ⭐️ and is released under ${license} license`)
-    core.setOutput("stars", stars);
-    core.setOutput("license", license);
-  })
-})
-repoRequest.on("error", () => {
-  core.setFailed("Failed to fetch GitHub");
-})
-repoRequest.end()
+main()
