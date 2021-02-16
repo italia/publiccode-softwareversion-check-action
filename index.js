67a11b989279f5189d2b0bb9b217ea96b6322391
Andrea Stagi <stagi.andrea@gmail.com>
2021-02-16 10:43:27 +0100

    feature: main functionality

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const https = require("https")
+const core = require("@actions/core")
+const yaml = require('js-yaml');
+const fs = require('fs');
+const simpleGit = require('simple-git');
 
-const githubToken = core.getInput('github_token');
-const repo = core.getInput('repo');
 
-const requestOptions = {
-  hostname: 'api.github.com',
-  path: `/repos/${repo}`,
-  headers: { 
-    'User-Agent': 'Mozilla/5.0',
+let main = async () => {
+  try {
+    const git = simpleGit();
+    const tag = (await git.tags()).latest
+    const publiccode = 'publiccode.yml'
+    let docContent = fs.readFileSync(publiccode, 'utf8')
+    const doc = yaml.load(docContent)
+
+    core.info(`Current tag is ${tag}`)
+
+    if (tag && doc.softwareVersion !== tag) {
+      core.setOutput("version", tag);
+      git.addConfig('user.name', core.getInput('gitname')) 
+      git.addConfig('user.email', core.getInput('gitmail'))
+      docContent = docContent.replace(/softwareVersion:.*/, `softwareVersion: ${tag}`)
+      console.log(docContent)
+      fs.writeFileSync(publiccode, docContent, 'utf8')
+      git.add('./*').commit(`feat: bump ${publiccode} to version ${tag}`)
+      throw `Current ${publiccode} should contain ${tag} version`
+    }
   }
-};
-if (githubToken) {
-  console.log("Use GITHUB_TOKEN to get release data.");
-  requestOptions.headers['Authorization'] = `token: ${githubToken}`
-} else {
-  console.log("GITHUB_TOKEN is not available. Subsequent GitHub API call may fail due to API limit.");
+  catch (error) {
+    core.setFailed(error)
+  }
 }
 
-const repoRequest = https.request(requestOptions, res => {
-  let responseData = '';
-  res.on('data', (d) => {
-    responseData += d;
-  })
-  res.on('end', () => {
-    const response = JSON.parse(responseData);
-    const stars = response.stargazers_count;
-    const license = response.license.name;
-    console.log(`Repo has ${stars} ⭐️ and is released under ${license} license`)
-    core.setOutput("stars", stars);
-    core.setOutput("license", license);
-  })
-})
-repoRequest.on("error", () => {
-  core.setFailed("Failed to fetch GitHub");
-})
-repoRequest.end()
+main()
